42e628cfa7c1b7251e0bc44ec2efd4f6ae33da8e
aadeshchendake <103639239+aadeshchendake@users.noreply.github.com>
2023-05-23 15:27:42 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,8 @@
-backend "azurerm" 
-{
-  resource_group_name  = "tamopstfstates"
-  storage_account_name = "tfstatedevops"
-  container_name       = "terraformgithubexample"
-  key                  = "terraformgithubexample.tfstate"
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tamopstfstates"
+    storage_account_name = "tfstatedevops"
+    container_name       = "terraformgithubexample"
+    key                  = "terraformgithubexample.tfstate"
+  }
 }
