1a0823c3d182f4eeedde69d337a6e6b8d375b8e7
aadeshchendake <aadeshc.in@mouritech.com>
2023-05-23 14:40:56 +0530

    infra code

---
+++ terraform/backend.tfstate.tf
@@ -0,0 +1,7 @@
+backend "azurerm" 
+{
+  resource_group_name  = "tamopstfstates"
+  storage_account_name = "tfstatedevops"
+  container_name       = "terraformgithubexample"
+  key                  = "terraformgithubexample.tfstate"
+}
