2697f672c5dcb538c71df69064e54b5dcee41f32
aadeshchendake <103639239+aadeshchendake@users.noreply.github.com>
2023-05-24 11:57:17 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,21 @@
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
   backend "azurerm" {
     resource_group_name  = "tamopstfstates"
     storage_account_name = "tfstatedevops"
     container_name       = "terraformgithubexample"
     key                  = "terraformgithubexample.tfstate"
   }
-}
- 
-provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "=3.0.0"
-  features {}
 }
  
 data "azurerm_client_config" "current" {}
