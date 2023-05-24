8c2eabbfd4acfa01c23bb0b4afc3c1212823a165
aadeshchendake <103639239+aadeshchendake@users.noreply.github.com>
2023-05-24 17:34:41 +0530

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tamopstfstates"
-    storage_account_name = "tfstatedevops"
-    container_name       = "terraformgithubexample"
-    key                  = "terraformgithubexample.tfstate"
-  }
-}
