daaef2504eb1ef8a22513d581dc9c67d04864814
aadeshchendake <103639239+aadeshchendake@users.noreply.github.com>
2023-05-26 12:49:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,16 +24,16 @@ resource "azurerm_resource_group" "aadeshrg" {
 
 #Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "tamops-vnet"
+  name                = "aadeshvnet"
   address_space       = ["192.168.0.0/16"]
   location            = "eastus2"
-  resource_group_name = azurerm_resource_group.tamops.name
+  resource_group_name = azurerm_resource_group.aadeshrg.name
 }
 
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.tamops.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = azurerm_resource_group.aadeshrg.name
+  virtual_network_name = azurerm_virtual_network.aadeshvnet.name
   address_prefix       = "192.168.0.0/24"
 }
